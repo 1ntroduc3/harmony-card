0ce1e9b1d367824795aa5b3d19bd7a3a5fc7b3d2
MHA <mattijs.hoogantink@ivengi.com>
2020-12-04 22:02:57 +0100

    chore: update card version

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 import { HarmonyButtonConfig } from "./types";
 
-export const CARD_VERSION = '1';
+export const CARD_VERSION = '2';
 
 export const DEFAULT_BUTTONS: { [key: string]: HarmonyButtonConfig } = {
     '0': {
